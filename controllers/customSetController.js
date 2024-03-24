dceb91eb228806fef129cca4f8178bec9abd6f08
S87 <S.Miller87@gmx.net>
2024-03-24 13:56:08 +0100

    Set the default mode to actually delete posts, instead of simply setting a deletion flag

--- controllers/customSetController.js
+++ controllers/customSetController.js
@@ -8,7 +8,56 @@ class CustomSetController {
         if (!this._setInstance)
             throw Error(`No custom set with the ID ${setId} was found!`);
 
+        this.#createToolbarLinks();
         this.#createSetUi();
+    }
+
+    #createToolbarLinks() {
+        const menu = document.querySelector("#nav > menu.secondary");
+        menu.innerHTML = "";
+
+        const items = [
+            { text: 'List', href: '/post_sets' },
+            { text: 'New', href: '/post_sets/new' },
+            { text: 'Help', href: '/help/sets' },
+            { text: 'Mine', href: `/post_sets?search[creator_id]=${UserHelper.getCurrentUserId()}`, hidden: !!UserHelper.getCurrentUserId() },
+            { text: '|', type: 'seperator' },
+            { text: 'Invites', href: '/post_set_maintainers' },
+            { text: 'Posts', href: `/posts?tags=custom_set:${this._setInstance.getId()}` },
+            { text: 'Edit', href: `/custom_sets/${this._setInstance.getId()}/edit` },
+            { text: 'Edit Posts', href: `/custom_sets/${this._setInstance.getId()}/post_list` },
+            { text: 'Delete', clickHandler: this.#onDeleteSetClicked.bind(this), confirm: 'Are you sure you want to delete this set?' }
+        ];
+
+        items.forEach(item => {
+            const li = document.createElement('li');
+
+            if (item.type !== "seperator") {
+                const a = document.createElement('a');
+                a.href = item.href || "#";
+
+                if (item.confirm && item.clickHandler)
+                    a.addEventListener("click", event => {
+                        if (confirm(item.confirm) === true)
+                            item.clickHandler(event);
+                    });
+
+                if (!item.confirm && item.clickHandler)
+                    a.addEventListener("click", item.clickHandler);
+
+                a.textContent = item.text;
+
+                li.appendChild(a);
+            } else {
+                li.innerText = item.text;
+            }
+            menu.appendChild(li);
+        });
+    }
+
+    #onDeleteSetClicked() {
+        this._setInstance.delete();
+        document.location.assign(`/post_sets?search[creator_id]=${UserHelper.getCurrentUserId()}`);
     }
 
     #createSetUi() {
